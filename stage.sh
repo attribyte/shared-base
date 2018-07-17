b2cb5ebe517a9f79cd3ae5b1c5c9b262bb69d182
Matt Hamer <matt@attribyte.com>
2018-07-17 12:26:57 -0400

    Remove old build files

--- stage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-VERSION="1.0.2"
-cp attribyte-shared-base-1.0.pom dist/lib/attribyte-shared-base-${VERSION}.pom
-cd dist/lib
-gpg -ab attribyte-shared-base-${VERSION}.pom
-gpg -ab attribyte-shared-base-${VERSION}.jar
-gpg -ab attribyte-shared-base-${VERSION}-sources.jar
-gpg -ab attribyte-shared-base-${VERSION}-javadoc.jar
-jar -cvf ../bundle.jar *
